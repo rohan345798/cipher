88e65d35e31e2b46b1afbd1f9e6f764270802c1e
yashneil <yashneil@outlook.com>
2015-12-20 10:57:00 -0500

    This is a message I typed

--- decrypt1.py
+++ decrypt1.py
@@ -1,1 +1,5 @@
-def decrypt(m, key):pass
+# decrypt will decrypt a message(m) with a key(key)
+def decrypt(encrypt, key):
+# decrypt will take the message and change it to the regular alphabet with the key
+# it will keep doing this until the message is decrypted
+        pass 
