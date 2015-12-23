8a863a3aa80866e2e97499f345cb784475c87b75
Rohan <rohanchakraborty123@gmail.com>
2015-12-23 16:41:19 -0500

    done

--- encrypt1.py
+++ encrypt1.py
@@ -1,5 +1,11 @@
-#encrypt will encrypt a message(m) with a key(key)
-def encrypt(m,key):
-#encrypt will take the key and change first letter of the message with the key
-#encrypt will go to the next letter and change and it will keep doing until m has been encrypted
-	pass
+def encrypt(m, key):
+        a = ''
+        for i in m:
+                i = i.lower()
+                b = key[i]
+                a = a + b
+        return a
+
+
+d = {'a':'z','b':'y','c':'x','d':'w','e':'v','f':'u','g':'t','h':'s','i':'r', 'j':'q', 'k':'p','l':'o','m':'n','n':'m','o':'l','p':'k','q':'j','r':'i','s':'h','t':'h','u':'f','v':'e','w':'d','x':'c','y':'b','z':'a',' ':' '}
+print encrypt('I want mutton',d)
