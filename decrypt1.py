e8167c116c2071480db64c82f753b88fab64071c
yashneil <yashneil@outlook.com>
2015-12-23 18:33:27 -0500

    bshbch

--- decrypt1.py
+++ decrypt1.py
@@ -2,20 +2,16 @@
 def decrypt(msg, k):
 # decrypt will take the message and change it to the regular alphabet with the key
 # it will keep doing this until the message is decrypted
-      y = ''
-      for l in msg:
-        # covert to lowercase becuase it was giving error
-        l = l.lower()
-        print 'l :',l
-        v = k[l]
+    y=''
+    for i in msg:
+        print 'i :',i         
+        v = k[i]
         print 'v :',v
-        y = y + v
+        y= y + v
         print 'y :',y
 
-      return y
+    return y
 
-    
-key = {'a':'z','b':'y','c':'x','d':'w','e':'v','f':'u','g':'t','h':'s','i':'r', 'j':'q', 'k':'p','l':'o',
-       'm':'n','n':'m','o':'l','p':'k','q':'j','r':'i','s':'h','t':'h','u':'f','v':'e','w':'d','x':'c','y':'b','z':'a',' ':' '}
-someinput = raw_input('please enter a message :')
-print decrypt(someinput,key) 
+
+key = {'z':'a','p': 'b','i':'s','a':'x','':''}
+print decrypt('pizza',key)
