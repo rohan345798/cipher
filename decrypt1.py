f8147fc8d21359d3819dd0597d9f1febf028e5bd
Rohan <rohanchakraborty123@gmail.com>
2015-12-23 16:47:49 -0500

    dfs

--- decrypt1.py
+++ decrypt1.py
@@ -1,1 +1,10 @@
-def decrypt(m, key):pass
+# decrypt will decrypt a message(m) with a key(key)
+def decrypt(encrypt, key):
+# decrypt will take the message and change it to the regular alphabet with the key
+# it will keep doing this until the message is decrypted
+    l= len(encrypt)
+    while (l != 0):
+        if encrypt[l] == 'a':
+            encrypt[1] = 'z'
+            l = l - 1#this is a one on the left
+            return encrypt
