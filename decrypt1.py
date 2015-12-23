d9d2f6eaecfb11af65c045f68b527563b94c4a5d
yashneil <yashneil@outlook.com>
2015-12-23 16:37:49 -0500

    Blah Blah

--- decrypt1.py
+++ decrypt1.py
@@ -2,4 +2,9 @@
 def decrypt(encrypt, key):
 # decrypt will take the message and change it to the regular alphabet with the key
 # it will keep doing this until the message is decrypted
-        pass 
+    l= len(encrypt)
+    while (l != 0):
+        if encrypt[l] == 'a':
+            encrypt[1] = 'z'
+            l = l - 1#this is a one on the left
+            return encrypt
