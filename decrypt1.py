96e171626ea717e76de6b91eccd0ec734bdb2329
Rohan <rohanchakraborty123@gmail.com>
2015-12-23 19:41:31 -0500

    here you go

--- decrypt1.py
+++ decrypt1.py
@@ -3,15 +3,8 @@ def decrypt(msg, k):
 # decrypt will take the message and change it to the regular alphabet with the key
 # it will keep doing this until the message is decrypted
     y=''
-    for i in msg:
-        print 'i :',i         
+    for i in msg:    
         v = k[i]
-        print 'v :',v
         y= y + v
-        print 'y :',y
 
     return y
-
-
-key = {'z':'a','p': 'b','i':'s','a':'x','':''}
-print decrypt('pizza',key)
--- encrypt1.py
+++ encrypt1.py
@@ -6,6 +6,3 @@ def encrypt(m, key):
                 a = a + b
         return a
 
-
-d = {'a':'z','b':'y','c':'x','d':'w','e':'v','f':'u','g':'t','h':'s','i':'r', 'j':'q', 'k':'p','l':'o','m':'n','n':'m','o':'l','p':'k','q':'j','r':'i','s':'h','t':'h','u':'f','v':'e','w':'d','x':'c','y':'b','z':'a',' ':' '}
-print encrypt('I want mutton',d)
